deb5dab7b8dcbd7e25abf60ffe48c47d5f15832d
mancmelou <mancmelou@me.com>
2014-03-07 07:38:47 -0800

    Patterns in scala

--- small_bites/patterns/patterns.scala
+++ small_bites/patterns/patterns.scala
@@ -44,3 +44,43 @@ object :+: {
   case 'f' :+: "ooo" => println("Not me!")
   case 'f' :+: 'o' :+: rest => println("The rest is " + rest)
 }
+
+// multy arity extractions
+object Factor {
+  def factor(n: Int): List[Int] = n match {
+    case 1 => List.empty
+    case n => {
+      for (i <- 2 to n) {
+        if (n % i == 0) {
+          return i :: factor(n / i)
+        }
+      }
+
+      return List(n, 1)
+    }
+  }
+
+  def unapplySeq(n: Int): Option[List[Int]] = {
+    return Some(factor(n))
+  }
+}
+
+// 120 =  2 * 2 * 2 * 3 * 5
+120 match {
+  case Factor(a, b, c) => println("Not me!")
+  case Factor(a, b, c, d, e) => println((a, c, e))
+}
+
+// pattern matchable lists can be created from scratch
+abstract class MyList[+A] {
+  def :*: [B >: A] (head: B) = new `:*:`(head, this)
+}
+
+case class :*:[A](val head: A, val tail: MyList[A]) extends MyList[A]
+case object MyNil extends MyList[Nothing]
+
+val l: MyList[Int] = 3 :*: 4 :*: 5 :*: MyNil
+
+l match {
+  case hd :*: tl => print(hd)
+}
